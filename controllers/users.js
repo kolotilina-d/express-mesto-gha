851b9542ef307a494721030fce68885cfb7f4534
Darya Kolotilina <colotilina.d@yandex.ru>
2023-08-16 16:47:56 +0200

    'token'

--- controllers/users.js
+++ controllers/users.js
@@ -100,7 +100,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      res.send(token);
+      res.send({ token });
     })
     .catch((err) => {
       next(err);
